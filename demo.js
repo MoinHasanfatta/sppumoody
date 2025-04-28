eec45a30b1da3f5f5154d65f24d5cae3c195b65f
Moin Hasanfatta <hasanfattamoin@gmail.com>
2025-04-28 14:02:43 +0530

    my demo project

---
+++ demo.js
@@ -0,0 +1,16 @@
+photo = document.getElementById('a')
+console.log(photo)
+b1 = document.getElementById('b')
+console.log(b1)
+b2 = document.getElementById('c')
+console.log(b2)
+
+function hii(){
+    photo.src="happy.jpeg"
+}
+
+function bye(){
+    photo.src="sad.jpeg"
+}
+
+
