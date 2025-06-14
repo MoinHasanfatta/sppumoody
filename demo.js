e7a9c6ec915f25d12688ba734f7f2884d384e7fe
Moin Hasanfatta <hasanfattamoin@gmail.com>
2025-06-14 22:44:21 +0530

    timer emoji

--- demo.js
+++ demo.js
@@ -7,10 +7,18 @@ console.log(b2)
 
 function hii(){
     photo.src="happy.jpeg"
+    setTimeout(()=>{
+        photo.src = "normal.jpeg"
+
+    },2000)
 }
 
 function bye(){
     photo.src="sad.jpeg"
+    setTimeout(()=>{
+        
+        photo.src = "normal.jpeg"
+    },3000)
 }
 
 
